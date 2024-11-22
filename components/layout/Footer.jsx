bdd68da27fee745e20f5e40543d85c979eb82e0c
jhs129 <john.schneider@hmhn.org>
2024-11-22 14:37:05 -0500

    fixed footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -28,9 +28,9 @@ const Footer = () => {
               <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/448d9c33c3956eb51cb814c764f6e52fe99a6e38fa6924b71d416c11fc73471d?placeholderIfAbsent=true&apiKey=7bf199de15724d268c1417f75ca31ce1" className="object-contain self-stretch my-auto aspect-[5.78] max-w-[1240px] min-w-[240px] w-[306px]" alt="Georgia CORE logo" />
             </div>
             <div className="flex w-full min-h-[24px] max-md:max-w-full"></div>
-            <h1 className="px-80 pt-4 w-full text-xl tracking-normal leading-7 text-center text-primaryLight max-md:px-5 max-md:max-w-full">
+            <p className="pt-4 w-full text-xl tracking-normal leading-7 text-center text-primaryLight max-md:px-5 max-md:max-w-full">
               Advancing Cancer Care through Partnerships and Innovation
-            </h1>
+            </p>
             <p className="pt-4 pr-5 pl-5 w-full text-base tracking-normal leading-6 text-center text-primaryLight max-md:pl-5 max-md:max-w-full">
               Georgia CORE is a public-private partnership that creates collaboration among the state's cancer organizations and institutions to connect more Georgians to quality,
               personalized cancer care. We welcome you to this one-of-a-kind online information center for all things related to cancer and survivorship care in Georgia.
@@ -43,12 +43,12 @@ const Footer = () => {
           <div className="flex flex-col flex-1 shrink self-stretch px-4 pb-4 my-auto w-full basis-0 max-w-[1240px] min-w-[240px] max-md:max-w-full">
             <nav className="flex flex-wrap gap-9 items-start py-4 px-60 w-full text-xl tracking-normal leading-8 text-center text-white max-md:px-5 max-md:max-w-full">
               {footerLinks.map((link, index) => (
-                <a key={index} href="#" className="self-stretch px-4 py-2.5 border border-solid bg-accentGreen border-accentGreen">
+                <a key={index} href="#" className="w-full md:w-fit px-4 py-2.5 border border-solid bg-accentGreen border-accentGreen">
                   {link}
                 </a>
               ))}
             </nav>
-            <p className="px-56 w-full text-base text-center text-white max-md:px-5 max-md:max-w-full">
+            <p className="px-56 w-full text-base text-center text-primaryLight max-md:px-5 max-md:max-w-full">
               Copyright © 2005 - @ 2024 Georgia Center for Oncology Research and Education, Inc. All Rights Reserved.
             </p>
             <div className="flex flex-row mx-auto gap-2">
