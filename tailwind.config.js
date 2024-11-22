79b30aa26cf47e1288a8ef4f8fd43d463045472b
jhs129 <john.schneider@hmhn.org>
2024-11-22 14:01:13 -0500

    working version

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,16 @@ module.exports = {
     
     extend: {
       colors: {
-        primaryLight: '--var(--color-primary-light, #FFFFFF)',
-        secondaryLight: '--var(--color-secondary-light)',
-        primaryDark: '--var(--color-primary-dark, #000000)',
-        secondaryDark: '--var(--color-secondary-dark, #343A40)',
-        primaryAccent: '--var(--color-primary-accent, #FFD166)',
-        secondaryAccent: '--var(--color-secondary-accent, #06D6A0)',
-        tertiaryAccent: '--var(--color-tertiary-accent, #118AB2)',
-        accentTeal: '--var(--color-accent-teal, #06D6A0)',
-        accentRed: '--var(--color-accent-red, #EF476F)',
-        accentGreen: '--var(--color-accent-green, #06D6A0)',
+        primaryLight: 'var(--color-primary-light, #FFFFFF)',
+        secondaryLight: 'var(--color-secondary-light)',
+        primaryDark: 'var(--color-primary-dark, #000000)',
+        secondaryDark: 'var(--color-secondary-dark)',
+        primaryAccent: 'var(--color-primary-accent, #FFD166)',
+        secondaryAccent: 'var(--color-secondary-accent, #06D6A0)',
+        tertiaryAccent: 'var(--color-tertiary-accent, #118AB2)',
+        accentTeal: 'var(--color-accent-teal, #06D6A0)',
+        accentRed: 'var(--color-accent-red, #EF476F)',
+        accentGreen: 'var(--color-accent-green, #06D6A0)',
       },
       fontFamily: {
         sans: ['Lufga', 'sans-serif'],
